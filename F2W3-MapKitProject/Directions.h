5a52e5a1c502624c9f75fe55ee9e071ef452e217
lee-leonardo <leoleez@hotmail.com>
2014-05-22 12:40:44 -0700

    This is the most recent version of this product. I am in the process of developing a directions asset.

---
+++ F2W3-MapKitProject/Directions.h
@@ -0,0 +1,13 @@
+//
+//  Directions.h
+//  F2W3-MapKitProject
+//
+//  Created by Leonardo Lee on 5/22/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface Directions : MKDirections
+
+@end
---
+++ F2W3-MapKitProject/Directions.m
@@ -0,0 +1,13 @@
+//
+//  Directions.m
+//  F2W3-MapKitProject
+//
+//  Created by Leonardo Lee on 5/22/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+#import "Directions.h"
+
+@implementation Directions
+
+@end
